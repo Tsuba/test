884a800f65b9f31489107d8226c0128b7c35595e
Tsuba <tsubadri_77@hotmail.com>
2016-12-29 12:21:28 +0100

    Update solution.ts

--- solution.ts
+++ solution.ts
@@ -7,41 +7,52 @@ import { Hero } from './hero';
   template: `
     <div *ngIf="hero">
     <div >
-      <div>
-        {{firstOne}}
-        <button (click)="change(firstOne)">Change</button>
-      </div>
-      <div>
-        {{secondOne}}
-        <button (click)="change(secondOne)">Change</button>
-      </div>
       
-      <div [ngSwitch]="firstOne">
-        <div *ngSwitchCase="null">
-          <h2 >{{hero.name}} details!</h2>
+      <div [ngSwitch]="firstSelected">
+        <div *ngSwitchCase="true">
+          <h2 >{{firstOne.name}} details 1!</h2>
+          <div><label>id: </label>{{firstOne.id}}</div>
+          <div >
+            <label>name: </label>
+            <div>{{firstOne.name}}</div>
+            <button (click)="change(firstOne)">Change</button>
+          </div>
+        </div>
+        <div *ngSwitchCase="false">
+          <h2 >{{hero.name}} details 1!</h2>
           <div><label>id: </label>{{hero.id}}</div>
           <div >
             <label>name: </label>
             <div>{{hero.name}}</div>
-            <button (click)="save(hero.name)">Save</button>
+            <button (click)="save(hero)">Save</button>
           </div>
         </div>
       </div>
-      <div [ngSwitch]="firstOne">
-        <div *ngSwitchCase="!null">
-          <h2 >{{hero.name}} details!</h2>
+      <div *ngIf="firstSelected">
+      <div [ngSwitch]="secondSelected">
+        <div *ngSwitchCase="true">
+          <h2 >{{secondOne.name}} details 2!</h2>
+          <div><label>id: </label>{{secondOne.id}}</div>
+          <div >
+            <label>name: </label>
+            <div>{{secondOne.name}}</div>
+            <button (click)="change(secondOne)">Change</button>
+          </div>
+        </div>
+        <div *ngSwitchCase="false">
+          <h2 >{{hero.name}} details 2!</h2>
           <div><label>id: </label>{{hero.id}}</div>
           <div >
             <label>name: </label>
             <div>{{hero.name}}</div>
-            <button (click)="save(hero.name)">Save</button>
+            <button (click)="save(hero)">Save</button>
           </div>
         </div>
+      </div>
       </div>
       
     </div>
     </div>
-
   `
 })
 export class HeroDetailComponent {
@@ -49,26 +60,30 @@ export class HeroDetailComponent {
   hero: Hero;
   firstOne : string ;
   secondOne : string;
+  firstSelected : boolean = false;
+  secondSelected : boolean = false;
+  
   
   change(data : string){
+
     if(data === this.firstOne){
       this.firstOne = null
+      this.firstSelected = false
     }
     else {
       this.secondOne = null
+      this.secondSelected = false
     }
   }
   
   save(data : Hero){
-    console.log(this.firstOne)
     if(this.firstOne==null){
       this.firstOne = data
+      this.firstSelected = true
     }
     else if(this.secondOne==null){
       this.secondOne = data
+      this.secondSelected = true
     }
-    console.log(this.firstOne);
-    this.saved = true;
   }
 }
-
